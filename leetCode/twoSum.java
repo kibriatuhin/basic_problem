860b038d6d094ea28d3435c8223fdf703a0e0625
kibriatuhin <tuinn78@gmail.com>
2022-09-03 14:01:59 +0600

    push in github

---
+++ leetCode/twoSum.java
@@ -0,0 +1,23 @@
+package leetCode;
+
+public class twoSum {
+    static void   twoSum(int[] nums , int target){
+        //int[] array = new int[2];
+        for (int i=0 ; i<nums.length ; i++){
+            for (int j =i+1 ; j< nums.length ; j++){
+                if (nums[i]+nums[j] == target)
+                    System.out.println(i + " " + j);
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int[] array = {1,2,4,5,8};
+        twoSum(array,7);
+    }
+}
+
+class Practice{
+    static void   twoSum(int[] nums , int target){
+
+    }
+}
