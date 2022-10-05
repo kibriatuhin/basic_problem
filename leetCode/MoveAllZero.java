0d579b5154e67463a1216a36a814321416d58d95
kibriatuhin <tuinn78@gmail.com>
2022-10-05 23:57:01 +0600

    solved viva releted problem

--- leetCode/MoveAllZero.java
+++ leetCode/MoveAllZero.java
@@ -29,3 +29,27 @@ public class MoveAllZero {
         moveZeroes(array);
     }
 }
+
+class Test55{
+    public static void moveZeroes(int[] nums){
+        int i=0,j=0,count =0;
+        while (i<nums.length){
+            if (nums[i] !=0){
+                    nums[j++] = nums[i++];
+
+            }
+            else {
+                count++;
+                i++;
+            }
+
+        }
+        for (int k = nums.length - count ; k< nums.length ; k++){
+            nums[k] = 0;
+        }
+    }
+    public static void main(String[] args) {
+        int[] array = {1,2};
+        moveZeroes(array);
+    }
+}
